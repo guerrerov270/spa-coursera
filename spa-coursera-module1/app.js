6c42a3596b6f3bb7b6d8d69be5208a8acf1bc737
Julian Guerrero <jguerrero089@gmail.com>
2020-09-12 18:39:05 -0500

    assignement 1

---
+++ spa-coursera-module1/app.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject= ['$scope'];
+function LunchCheckController ($scope){
+	$scope.dishesList= '';
+	$scope.validationMessage='';
+	$scope.check=false;
+
+$scope.checkLunch = function () {
+
+if ($scope.dishesList.trim().length === 0) {
+        $scope.isEmpty = true;
+    } else {
+        $scope.checked = true;
+        $scope.isEmpty = false;
+        var dishesListArray = $scope.dishesList.split(',');
+        if (arrayDishesWithoutEmptys.length <= 3) {
+            $scope.validationMessage = 'Enjoy!';
+        } else {
+            $scope.validationMessage = 'Too much!';
+        }
+    }
+
+};
+		
+}
+})();
